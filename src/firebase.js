6626111d1e59799a947bffb99948519c84adf2bf
SEOKWON-YANG <namugocps@gmail.com>
2021-12-20 23:32:10 +0900

    twitter #2
    twitter #2
    
    - firebase 설정

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBYRSWV7a0KdRZ-QN9TafShiXhiakiGTdU",
+    authDomain: "twitter-4d7b9.firebaseapp.com",
+    projectId: "twitter-4d7b9",
+    storageBucket: "twitter-4d7b9.appspot.com",
+    messagingSenderId: "615448347530",
+    appId: "1:615448347530:web:b5e8a497ee6bbf06343e73"
+  };
+
+export default firebase.initializeApp(firebaseConfig);

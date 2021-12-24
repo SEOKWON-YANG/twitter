1d44534dce694cd79005aabab695ccbc3d83cf89
SEOKWON-YANG <namugocps@gmail.com>
2021-12-24 23:21:19 +0900

    twitter #6
    twitter #6
    
    - 날짜 적용

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
-
+import {useState} from "react";
+import AppRouter from "./Router"
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <div>App</div>
+    <>
+    <AppRouter isLoggedIn={isLoggedIn} />
+    <footer>&copy; {new Date().getFullYear()} Twitter</footer>
+    </>
   );
 }
 
